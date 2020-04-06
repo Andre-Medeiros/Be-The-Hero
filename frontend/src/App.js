4f2d6846b37b201952cd04f64ab9df4f126c7578
Andre-Medeiros <andre_luiz_1996@hotmail.com>
2020-04-06 20:59:17 -0300

    Projeto concluido

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './global.css';
+import Logon from './pages/Logon';
+import Routes from './routes';
+import { Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
